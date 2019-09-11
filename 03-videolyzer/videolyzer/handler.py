0ff1f42a5ba520498002989c5848485fe4b82ecd
fkkong <fkk01@hotmail.com>
2019-09-11 22:35:10 +0800

    Publish job completion to SNS topic and handle via lambda func.

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,7 +1,8 @@
+import os
+
 import urllib
 
 import boto3
-
 def start_label_detection(bucket, key):
     rekognition_client = boto3.client('rekognition')
     response = rekognition_client.start_label_detection(
@@ -10,20 +11,33 @@ def start_label_detection(bucket, key):
                 'Bucket': bucket,
                 'Name': key
             }
+        },
+        NotificationChannel={
+            'SNSTopicArn': os.environ['REKOGNITION_SNS_TOPIC_ARN'],
+            'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
         })
 
     print(response)
+
+    # while not done:
+    #   time.sleep(10)
+    #   done = check_job_status()
 
     return
 
 
 def start_processing_video(event, context):
-    #print(event)
     for record in event['Records']:
         start_label_detection(
             record['s3']['bucket']['name'],
             urllib.parse.unquote_plus(record['s3']['object']['key'])
         )
 
+
+    return
+
+def handle_label_detection(event, context):
+
+    print(event)
 
     return
