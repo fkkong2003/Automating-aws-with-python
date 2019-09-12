400804f95427ee769f09f969bbea73059a7b600d
fkkong <fkk01@hotmail.com>
2019-09-12 22:18:17 +0800

    Save data from get_label_detection to dynamodb.

--- 02-notifon/scale-up.py
+++ 02-notifon/scale-up.py
@@ -3,3 +3,4 @@ session = boto3.Session(profile_name='pythonautomation')
 as_client = session.client('autoscaling')
 
 as_client.execute_policy(AutoScalingGroupName='Notifon Example Group', PolicyName='Scale Up')
+''
